53952841bbe0182e5c843347d1fa053de94991f4
smithray113 <maia73marquardtzyp@hotmail.com>
2022-02-14 20:26:01 +0200

    Added more MarketService tests

--- Tests/MarketServiceTests.cs
+++ Tests/MarketServiceTests.cs
@@ -10,17 +10,16 @@ public class MarketServiceTests
 {
     public readonly string FromDate;
     public readonly string ToDate;
+    public readonly string ToDateExtension;
+    public readonly string ToDateNullExtension;
+
     public readonly IMarketService _marketService;
-    public readonly List<MarketChartPoint>? _marketChartPoints;
 
     public MarketServiceTests()
     {
         var date = new DateTimeOffset(2021, 1, 1, 12, 0, 0, 0, new TimeSpan(0, 0, 0));
 
-        FromDate = DateHelper.DateTimeOffsetToDate(date);
-        ToDate = DateHelper.DateTimeOffsetToDate(date.AddDays(4));
-
-        _marketChartPoints = new List<MarketChartPoint> {
+        var marketChartPoints = new List<MarketChartPoint> {
             new MarketChartPoint
             {
                 Date = date,
@@ -58,11 +57,46 @@ public class MarketServiceTests
             },
         };
 
+        var marketChartPointsExtension = new List<MarketChartPoint> {
+            new MarketChartPoint
+            {
+                Date = date.AddDays(5),
+                Price = 50m,
+                MarketCap = 100m,
+                TotalVolume = 50m,
+            },
+            new MarketChartPoint
+            {
+                Date = date.AddDays(6),
+                Price = 40m,
+                MarketCap = 100m,
+                TotalVolume = 40m,
+            },
+            new MarketChartPoint
+            {
+                Date = date.AddDays(7),
+                Price = 30m,
+                MarketCap = 100m,
+                TotalVolume = 30m,
+            },
+        };
+
+        List<MarketChartPoint>? marketChartPointsNullExtension = null;
+
         var logger = new NullLogger<MarketService>();
+
+        FromDate = DateHelper.DateTimeOffsetToDate(date);
+        ToDate = DateHelper.DateTimeOffsetToDate(date.AddDays(4));
+        ToDateExtension = DateHelper.DateTimeOffsetToDate(date.AddDays(7));
+        ToDateNullExtension = DateHelper.DateTimeOffsetToDate(date.AddDays(10));
 
         var marketStore = new Mock<IMarketStore>(MockBehavior.Strict);
         marketStore.Setup(x => x.GetMarketChartByDateRange(FromDate, ToDate))
-            .ReturnsAsync(_marketChartPoints);
+            .ReturnsAsync(marketChartPoints);
+        marketStore.Setup(x => x.GetMarketChartByDateRange(ToDate, ToDateExtension))
+            .ReturnsAsync(marketChartPointsExtension);
+        marketStore.Setup(x => x.GetMarketChartByDateRange(ToDateExtension, ToDateNullExtension))
+            .ReturnsAsync(marketChartPointsNullExtension);
 
         _marketService = new MarketService(logger, marketStore.Object);
     }
@@ -74,6 +108,14 @@ public class MarketServiceTests
 
         result.Should().NotBeNull();
         result.Should().Be(3);
+    }
+
+    [Fact]
+    public async Task TestLongestDownwardTrendNoDataReturnsNull()
+    {
+        var result = await _marketService.GetLongestDownwardTrend(ToDateExtension, ToDateNullExtension);
+
+        result.Should().BeNull();
     }
 
     [Fact]
@@ -89,6 +131,14 @@ public class MarketServiceTests
     }
 
     [Fact]
+    public async Task TestHighestTradingVolumeNoDataReturnsNull()
+    {
+        var result = await _marketService.GetHighestTradingVolume(ToDateExtension, ToDateNullExtension);
+
+        result.Should().BeNull();
+    }
+
+    [Fact]
     public async Task TestBestBuyAndSellDatesReturnsValue()
     {
         var result = await _marketService.GetBestBuyAndSellDates(FromDate, ToDate);
@@ -98,5 +148,21 @@ public class MarketServiceTests
         result?.SellDate.Should().NotBeNull();
         result?.BuyDate.Should().Be("2021-01-04");
         result?.SellDate.Should().Be("2021-01-05");
+    }
+
+    [Fact]
+    public async Task TestBestBuyAndSellDatesOnlyDecreasingReturnsNull()
+    {
+        var result = await _marketService.GetBestBuyAndSellDates(ToDate, ToDateExtension);
+
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task TestBestBuyAndSellDatesNoDataReturnsNull()
+    {
+        var result = await _marketService.GetBestBuyAndSellDates(ToDateExtension, ToDateNullExtension);
+
+        result.Should().BeNull();
     }
 }
