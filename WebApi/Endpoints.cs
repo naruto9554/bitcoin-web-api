5a32ebb5e5c9edc690b1fb3024a6303795523ab8
smithray113 <maia73marquardtzyp@hotmail.com>
2022-11-16 08:29:21 +0200

    Add examples for each parameter

--- WebApi/Endpoints.cs
+++ WebApi/Endpoints.cs
@@ -1,9 +1,14 @@
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.OpenApi.Any;
 
 public static class Endpoints
 {
+    private static readonly OpenApiString ExampleFromDate = new OpenApiString("2022-01-01");
+    private static readonly OpenApiString ExampleToDate = new OpenApiString("2022-01-31");
+
     public static IEndpointRouteBuilder ConfigureEndpoints(this IEndpointRouteBuilder endpoints)
     {
         endpoints.MapGet("/longestdownwardtrend", async (IMarketService service, string fromDate, string toDate) =>
@@ -18,6 +23,8 @@ public static class Endpoints
         .WithOpenApi(operation =>
         {
             operation.Summary = "Get longest downward trend in days between given dates";
+            operation.Parameters.First().Example = ExampleFromDate;
+            operation.Parameters.Last().Example = ExampleToDate;
             return operation;
         });
 
@@ -34,6 +41,8 @@ public static class Endpoints
         .WithOpenApi(operation =>
         {
             operation.Summary = "Get the date with the highest trading volume between given dates";
+            operation.Parameters.First().Example = ExampleFromDate;
+            operation.Parameters.Last().Example = ExampleToDate;
             return operation;
         });
 
@@ -50,6 +59,8 @@ public static class Endpoints
         .WithOpenApi(operation =>
         {
             operation.Summary = "Get pair of dates when it is best to buy and sell between given dates";
+            operation.Parameters.First().Example = ExampleFromDate;
+            operation.Parameters.Last().Example = ExampleToDate;
             return operation;
         });
 
