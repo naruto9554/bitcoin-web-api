f4f4f62d6e262b1e0d96c0495e76e127b00cd81a
smithray113 <maia73marquardtzyp@hotmail.com>
2024-02-28 23:09:04 +0200

    Restructure folders

--- App/Extensions/DateExtensions.cs
+++ App/Extensions/DateExtensions.cs
@@ -1,7 +1,12 @@
 using System;
 
-public static class DateOnlyExtensions
+public static class DateExtensions
 {
+    public static DateOnly ToDateOnly(this DateTimeOffset offset)
+    {
+        return new DateOnly(offset.Year, offset.Month, offset.Day);
+    }
+
     public static long ToUnixTimestamp(this DateOnly date)
     {
         var offset = new DateTimeOffset(date.Year, date.Month, date.Day, 0, 0, 0, TimeSpan.Zero);
